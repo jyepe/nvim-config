962ddc525f5b7bd991be32693969160e2e781cb2
MOTHERSMILK\yepej <julianyepe94@proton.me>
2026-07-11 15:36:37 -0400

    feat: set scrolloff to 8 for more context

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Default options: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+-- Additional personal options
+vim.opt.scrolloff = 8 -- More context lines when scrolling (LazyVim default: 4)
